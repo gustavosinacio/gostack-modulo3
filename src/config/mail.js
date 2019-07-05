b1c683d78fd31fb4f1b60df18479bf99a20a7325
gustavo <gsinacio94@gmail.com>
2019-07-05 20:02:33 -0300

    Finishing email sending

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '77c0a210066036',
+    pass: '7f934816f696f0',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@GoBarber.com>',
+  },
+};
