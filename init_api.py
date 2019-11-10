8859219aca096a6ce8cde5cafa4d37bee1f6bd5e
Gustavo Inacio <gsinacio94@gmaill.com>
2019-11-10 12:02:53 -0300

    Changes init code's name to init_api.py

--- init_api.py
+++ init_api.py
@@ -3,3 +3,5 @@ import os
 os.system('docker run --name postgres -p 5433:5432 -d -t postgres')
 os.system('docker run --name mongobarber -p 27017:27017 -d  -t mongo')
 os.system('docker run --name redisbarber -p 6379:6379 -d -t redis:alpine')
+
+os.system('docker exec -it postgres psql -U postgres -c "CREATE DATABASE gobarber;"')
