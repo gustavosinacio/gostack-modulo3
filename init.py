de5707112d35c010a83dbe61b127a0bab57ce3d3
Gustavo Inacio <gsinacio94@gmaill.com>
2019-11-10 12:09:42 -0300

    Improves init.py and readme

--- init.py
+++ init.py
@@ -5,3 +5,5 @@ os.system('docker run --name mongobarber -p 27017:27017 -d  -t mongo')
 os.system('docker run --name redisbarber -p 6379:6379 -d -t redis:alpine')
 
 os.system('docker exec -it postgres psql -U postgres -c "CREATE DATABASE gobarber;"')
+
+os.system(' npx sequelize-cli db:migrate')
